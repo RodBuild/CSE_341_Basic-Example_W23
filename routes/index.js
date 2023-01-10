7d700006b0928fa2741917eaf451aea93cfd07a5
Rodrigo Rodriguez <rodrigo.edwin@hotmail.com>
2023-01-10 10:13:07 -0700

    default message added

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/', (req, res, next) => {
+    res.status(200).send('Sarah Birch');
+});
 router.use('/contacts', require('./contacts'));
 
 module.exports = router;
